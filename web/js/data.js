89485b26a78a469c0f1b9ec8a022d1eaf824a4e4
Thao Lai <thanh.thao14190@gmail.com>
2026-08-12 14:07:58 +0700

    data: cache-bust + verbose error diagnosis

--- web/js/data.js
+++ web/js/data.js
@@ -11,10 +11,20 @@ APP.data = (function () {
     if (!url) {
       return Promise.reject(new Error('API_URL is not configured. Edit web/js/config.js.'));
     }
-    return fetch(url, { method: 'GET', redirect: 'follow' })
+    // Cache-bust so the browser doesn't reuse an old redirect target that may have expired.
+    var bust = url + (url.indexOf('?') === -1 ? '?' : '&') + 't=' + Date.now();
+    return fetch(bust, { method: 'GET', redirect: 'follow', cache: 'no-store' })
       .then(function (res) {
-        if (!res.ok) { throw new Error('HTTP ' + res.status); }
-        return res.json();
+        if (!res.ok) {
+          throw new Error('HTTP ' + res.status + ' from ' + res.url);
+        }
+        return res.text().then(function (txt) {
+          try {
+            return JSON.parse(txt);
+          } catch (e) {
+            throw new Error('Backend did not return JSON. First 120 chars: ' + txt.slice(0, 120));
+          }
+        });
       })
       .then(function (payload) {
         APP.state.lessons = (payload && payload.lessons) || [];
